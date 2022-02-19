de0354cb85e1b8bb0b738ad80588b359934ca02f
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-02-19 15:54:50 +0100

    changed env variable SERVER_PORT to PORT

--- src/server/routers/robotsRouter.js
+++ src/server/routers/robotsRouter.js
@@ -8,8 +8,17 @@ router.get("/robots", async (req, res) => {
   res.json({ robots });
   res.status(200);
 });
-router.get("/robots/:idRobot");
-router.post("/robots/create");
+router.get(`/robots/:idRobot`, async (req, res) => {
+  const robot = await Robot.find();
+  res.json({ robot });
+  res.status(200);
+});
+router.post("/robots/create", async (req, res) => {
+  const newRobot = req.body;
+  const createdRobot = await Robot.create(newRobot);
+  res.status(201);
+  res.json(createdRobot);
+});
 router.put("/robots/update");
 router.delete("/robots/delete/:idRobot");
 
